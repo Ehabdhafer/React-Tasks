8caef9bfb4110f4b6ded2a4487d2b7cb8d3b0754
unknown <ehabzaferabed@gmail.com>
2024-04-03 14:09:48 +0300

    edit hero section

--- designing_tasks/src/components/hero.jsx
+++ designing_tasks/src/components/hero.jsx
@@ -2,8 +2,10 @@ const Hero = () => {
   return (
     <div className=" xl:pl-28 mt-2 flex flex-wrap justify-between h-[500px] bg-neutral-50 ">
       <div className="flex flex-wrap flex-col justify-center gap-9">
-        <p className="text-7xl w-[40rem]">Next generation digital banking</p>
-        <p className=" text-xl w-[40rem]">
+        <p className="text-7xl xl:w-[40rem] lg:w-96">
+          Next generation digital banking
+        </p>
+        <p className=" text-xl xl:w-[34rem] lg:w-96">
           Take your financial life online. Your Easybank account will be a
           one-stop-shop for spending, saving, budgeting, investing, and much
           more.
@@ -18,7 +20,7 @@ const Hero = () => {
       <img
         src="https://tailwindpractice-easybank.netlify.app/images/image-mockups.png"
         alt="hero"
-        className="h-full object-none xl:w-[40rem]"
+        className="h-full object-none xl:w-[40rem] lg:w-[35%]"
       />
     </div>
   );
