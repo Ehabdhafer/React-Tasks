e394df448da6f48c93dfcb20e03559c2a544c733
unknown <ehabzaferabed@gmail.com>
2024-04-17 19:39:36 +0300

    add hero section

---
+++ designing_tasks/src/components/hero2.jsx
@@ -0,0 +1,16 @@
+const Hero2 = () => {
+  return (
+    <div className="h-[32rem] bg-sky-400 flex flex-wrap items-center flex-col pt-8">
+      <div className=" text-white font-semibold text-6xl">WE ARE CREATIVES</div>
+      <img
+        src="
+        https://pngimg.com/uploads/orange/orange_PNG748.png
+        "
+        alt="orange"
+        className="size-[30rem]"
+      />
+    </div>
+  );
+};
+
+export default Hero2;
--- designing_tasks/src/tasks/sunnyside.jsx
+++ designing_tasks/src/tasks/sunnyside.jsx
@@ -1,9 +1,11 @@
 import Header2 from "../components/header2";
+import Hero2 from "../components/hero2";
 
 const SunnySide = () => {
   return (
     <div>
       <Header2 />
+      <Hero2 />
     </div>
   );
 };
