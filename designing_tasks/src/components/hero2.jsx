a0491fd0e1bbe88e092d08240f9cfa9b367ecf95
unknown <ehabzaferabed@gmail.com>
2024-04-18 16:57:44 +0300

    edit hero section

--- designing_tasks/src/components/hero2.jsx
+++ designing_tasks/src/components/hero2.jsx
@@ -1,14 +1,14 @@
 const Hero2 = () => {
   return (
-    <div className="h-[32rem] bg-sky-400 flex flex-wrap items-center flex-col pt-8">
+    <div className="h-[32rem] bg-sky-400 flex items-center flex-col pt-16 gap-[68px]">
       <div className=" text-white font-semibold text-6xl">WE ARE CREATIVES</div>
-      <img
-        src="
-        https://pngimg.com/uploads/orange/orange_PNG748.png
-        "
-        alt="orange"
-        className="size-[30rem]"
-      />
+      <div className="max-w-[40rem] max-h-[20rem] overflow-hidden">
+        <img
+          src="https://pngimg.com/uploads/orange/orange_PNG748.png"
+          alt="orange"
+          className="object-contain"
+        />
+      </div>
     </div>
   );
 };
